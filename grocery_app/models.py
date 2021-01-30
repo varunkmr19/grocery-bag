a6f588a958afecb12aa642830fe42b48cd0e8544
varunkmr19 <varun-kumar@live.com>
2021-01-30 19:46:06 +0530

    add models

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Item(models.Model):
+    ITEM_STATUS = (
+        ('PENDING', 'PENDING'),
+        ('BOUGHT', 'BOUGHT'),
+        ('NOT_AVAILABLE')
+    )
+    name = models.CharField(max_length=150)
+    quantity = models.CharField(max_length=150)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class GroceryList(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='list')
+    item = models.ForeignKey(
+        Item, on_delete=models.CASCADE, related_name='item_list')
+
+    def __str__(self):
+        return self.user.first_name
