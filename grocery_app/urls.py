d15d26ed748dbff4542e3a3dab3e0e594dcb07e1
varunkmr19 <varun-kumar@live.com>
2021-01-30 21:37:20 +0530

    add login and signup templates

--- grocery_app/urls.py
+++ grocery_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login', views.login_view, name='login'),
+    path('signup', views.signup_view, name='signup')
 ]
--- grocery_app/views.py
+++ grocery_app/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'grocery_app/layout.html')
+
+
+def login_view(request):
+    return render(request, 'grocery_app/login.html')
+
+
+def signup_view(request):
+    return render(request, 'grocery_app/signup.html')
