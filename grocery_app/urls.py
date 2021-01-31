779fab98eedad6363b7c6607744ed854444a40c9
varunkmr19 <varun-kumar@live.com>
2021-01-31 12:42:29 +0530

    add register view

--- grocery_app/urls.py
+++ grocery_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.LoginView.as_view(), name='login'),
-    path('signup', views.signup_view, name='signup'),
+    path('register', views.RegistrationView.as_view(), name='register'),
     path('logout', views.LogoutView.as_view(), name='logout')
 ]
--- grocery_app/views.py
+++ grocery_app/views.py
@@ -3,6 +3,8 @@ from django.views.generic import View
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.db import IntegrityError
+from . models import User
 
 # Create your views here.
 
@@ -37,5 +39,31 @@ class LogoutView(View):
         return HttpResponseRedirect(reverse('index'))
 
 
-def signup_view(request):
-    return render(request, 'grocery_app/signup.html')
+class RegistrationView(View):
+    def post(self, request):
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        username = request.POST['username']
+
+        # Ensure password matches confirmation
+        password = request.POST['password']
+        confirmation = request.POST['confirm-password']
+        if password != confirmation:
+            return render(request, 'grocery_app/register.html', {
+                'error': 'Passwords must match.'
+            })
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(
+                username, email, password,
+                first_name=first_name,
+                last_name=last_name
+            )
+            user.save()
+        except IntegrityError:
+            return render(request, 'grocery_app/register.html', {
+                'error': 'Username already taken.'
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse('index'))
